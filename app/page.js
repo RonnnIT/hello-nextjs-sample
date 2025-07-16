e7937ba6c1b7a65571ba51b8e2d59491e9d59ec4
OLCA-Ron-IT Web <132258131+RonnnIT@users.noreply.github.com>
2025-07-16 13:28:30 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,20 +8,9 @@ import Layout from "@/components/Layout";
 export default async function HomePage() {
   const user = await auth();
   return (
-    <HelloProvider auth={user}>
-      <Layout>
-        <LoggedIn>
-          <Hero/>
-        </LoggedIn>
-        <LoggedOut>
-          <Prompt/>
-          {/* [ ō Continue with Hellō ] */}
-          <ContinueButton theme="aware-static" hover="flare" providerHint={['google','github','gitlab']} />
-          {/* Default <ContinueButton color="black" theme="ignore-light" hover="pop" />  */}
-        </LoggedOut>
-        <Info/>
-      </Layout>
-    </HelloProvider>
+    <div>
+      hello
+    </div>
   );
 }
 
